db586293f52935c9b019a5d9dad118891a7b8f94
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-02-13 21:43:17 +0530

    Learning Context

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./BookList.css";
 import BookShow from "../BookShow/BookShow";
 
+import BooksContext from "../../context/books";
+
 const BookList = ({ books, onDeleteHandle, onEditHandle }) => {
+  const value = useContext(BooksContext);
   // console.log(books);
   // console.log(typeof books);
   return (
     <div className="book-list">
+      <h3>{value}</h3>
       {books.map((book) => (
         <BookShow
           key={book.id}
